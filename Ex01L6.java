dc4b15cd2607a78829afc10fd2dca7a7a7922b72
Logan-Michel-JF <38558603+Logan-Michel-JF@users.noreply.github.com>
2018-05-08 11:31:20 -0300

    inicio da for com vetor

---
+++ Ex01L6.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class Ex01L6 {
+
+	public static void main(String[] args) {
+		
+		String nomes[] = new String[25];
+		
+		for(int i = 0; i < 25; i++){
+			JOptionPane.showInputDialog(null, "Digite o Nome: ");
+		}
+		
+
+	}
+
+}
---
+++ Ex02L6.java
@@ -0,0 +1,9 @@
+
+public class Ex02L6 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
